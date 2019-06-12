08405d0f3924af4cda7b336aa4ab4cdbc5c8ea5c
Anton Lindroth <ntoonio@gmail.com>
2019-06-13 01:24:31 +0200

    Version mump to 0.0.4

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as fh:
-	install_requires = fh.read().splitlines()
-
 setuptools.setup(
     name="AlfonsSensor",
-    version="0.0.3.1",
+    version="0.0.4",
     author="Anton Lindroth",
     author_email="ntoonio@gmail.com",
     description="A package for sensors to interact with Alfons",
@@ -16,7 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ntoonio/AlfonsSensor.git",
     packages=setuptools.find_packages(),
-	install_requires=install_requires,
+	install_requires=[
+		"AlfonsIoT"
+	],
 	entry_points = {
 		"console_scripts": [
 			"alfons_sensor = AlfonsSensor.__main__:main",
